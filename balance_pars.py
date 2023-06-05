48e1ca03b6586286aa13fef6afbc7bc53d44d8b0
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-06-05 14:31:44 +1000

    Add classes

--- balance_pars.py
+++ balance_pars.py
@@ -1,36 +1,4 @@
-import random
-import time
-from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
-import pandas as pd
-import openpyxl
-import requests
-from bs4 import BeautifulSoup as BS
-# from selenium.webdriver.chrome.service import Service
-
-
-class Yahoo:
-
-    def __init__(self, report):
-        self.report = report
-
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        self.driver = webdriver.Chrome(
-            r'/Users/pavelseryi/PycharmProjects/pythonProject/Price_pars/chromedriver', options=options)
-
-    def get_url(self, ticker):
-        report_type = {'Income Statement': 'financials',
-                       'Balance Sheet': 'balance-sheet'}
-
-        url = f'https://finance.yahoo.com/quote/{ticker}/{report_type[self.report]}?p={ticker}'
-        return url
-
-    def get_company_name(self, url):
-        driver = self.driver
-        driver.get(url)
-        return driver.find_element(By.ID, 'quote-header-info').text.split('\n')[0]
+from classes import Yahoo
 
 
 def main():
@@ -40,7 +8,7 @@ def main():
 
     yahoo_income = Yahoo(report='Income Statement')
     url = yahoo_income.get_url('GM')
-    print(url)
+    print(yahoo_income.get_company_name(url))
 
 
 if __name__ == '__main__':
