a3b51a8d26ec8859080dc191df5fc60e82ee577e
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-06-07 00:05:56 +1000

    Add files via upload

--- balance_pars.py
+++ balance_pars.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from classes import Yahoo
 
 
@@ -6,9 +8,28 @@ def main():
                      'MX', 'L', 'SN', 'OL', 'ST', 'SW', 'VI', 'MI', 'IL', 'AT', 'MC', 'LS', 'AX', 'ME', 'AS', 'DE',
                      'BR', 'TO']
 
-    yahoo_income = Yahoo(report='Income Statement')
-    url = yahoo_income.get_url('GM')
-    print(yahoo_income.get_company_name(url))
+    yahoo_income = Yahoo(report='Balance Sheet')
+    url = yahoo_income.get_url('DE')
+
+    name_ticker = yahoo_income.get_company_name(url)
+    name = ' '.join(name_ticker.split()[:-1])
+    ticker = ''.join(name_ticker.split()[-1])
+
+    head = yahoo_income.get_head(url)
+
+    all_data = yahoo_income.get_all_data(url)
+    df = pd.DataFrame({name: [''], ticker: ['']})
+    for col_name in head:
+        df[col_name] = ['']
+
+    for i in range(3, len(all_data) + 3):
+        df.loc[i] = ['', ''] + all_data[i - 3]
+
+    df.to_excel('/Users/pavelseryi/PycharmProjects/pythonProject/balance_parser/Выгрузка тест.xlsx',
+                index=False)
+
+    # Поменять столбцы местами
+    # Убрать 2 пустую строку
 
 
 if __name__ == '__main__':
--- classes.py
+++ classes.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+import random
 
 
 class Yahoo:
 
     def __init__(self, report):
         self.report = report
+        self.url_check = {}
 
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
@@ -20,6 +23,50 @@ class Yahoo:
         return url
 
     def get_company_name(self, url):
-        driver = self.driver
-        driver.get(url)
-        return driver.find_element(By.ID, 'quote-header-info').text.split('\n')[0]
+        self.driver.get(url)
+        self.url_check[url] = True
+        return self.driver.find_element(By.ID, 'quote-header-info').text.split('\n')[0]
+
+    def get_head(self, url):
+        try:
+            check = self.url_check[url]
+            del check
+        except KeyError:
+            self.driver.get(url)
+
+        report_type = {'Income Statement': 5,
+                       'Balance Sheet': 4}
+
+        head = ['Breakdown']
+        for col_name in self.driver.find_elements(By.CLASS_NAME, 'BdB')[:report_type[self.report]]:
+            head.append(col_name.text)
+
+        return head
+
+    def get_all_data(self, url):
+        try:
+            check = self.url_check[url]
+            del check
+        except KeyError:
+            self.driver.get(url)
+
+        time.sleep(random.uniform(0.5, 1))
+        self.driver.find_element(By.CLASS_NAME, 'expandPf').click()
+
+        report_type = {'Income Statement': 5,
+                       'Balance Sheet': 4}
+
+        data = []
+        for row in self.driver.find_elements(By.CLASS_NAME, 'fi-row'):
+            names = [' '.join(row.text.split()[:-report_type[self.report]])]
+            nums = []
+
+            for num in row.text.replace(',', '').split()[-report_type[self.report]:]:
+                try:
+                    nums.append(float(num))
+                except ValueError:
+                    nums.append(num)
+
+            data.append(names + nums)
+
+        return data
