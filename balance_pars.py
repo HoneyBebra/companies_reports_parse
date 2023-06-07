de0114992ad6e674f49e7f8404f5b58e3dcb5533
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-06-07 17:56:10 +1000

    Add files via upload

--- balance_pars.py
+++ balance_pars.py
@@ -3,13 +3,9 @@ import pandas as pd
 from classes import Yahoo
 
 
-def main():
-    yahoo_tickers = ['DE', 'T', 'KS', 'SS', 'ME', 'HK', 'TW', 'JK', 'TO', 'IS', 'CO', 'KL', 'PA', 'SA', 'BK', 'SW',
-                     'MX', 'L', 'SN', 'OL', 'ST', 'SW', 'VI', 'MI', 'IL', 'AT', 'MC', 'LS', 'AX', 'ME', 'AS', 'DE',
-                     'BR', 'TO']
-
-    yahoo_income = Yahoo(report='Balance Sheet')
-    url = yahoo_income.get_url('DE')
+def get_data_yahoo(report, ticker):
+    yahoo_income = Yahoo(report=report)
+    url = yahoo_income.get_url(ticker)
 
     name_ticker = yahoo_income.get_company_name(url)
     name = ' '.join(name_ticker.split()[:-1])
@@ -18,18 +14,41 @@ def main():
     head = yahoo_income.get_head(url)
 
     all_data = yahoo_income.get_all_data(url)
-    df = pd.DataFrame({name: [''], ticker: ['']})
+    df = pd.DataFrame({name: [''], ticker: [''], report: ['']})
     for col_name in head:
         df[col_name] = ['']
 
     for i in range(3, len(all_data) + 3):
-        df.loc[i] = ['', ''] + all_data[i - 3]
+        df.loc[i] = ['', '', ''] + all_data[i - 3]
 
-    df.to_excel('/Users/pavelseryi/PycharmProjects/pythonProject/balance_parser/Выгрузка тест.xlsx',
-                index=False)
+    if report == 'Balance Sheet':
+        empty_list = [''] * len(df)
+        df['TTM'] = empty_list
+        df = df.iloc[:, [0, 1, 2, 3, 8, 7, 6, 5, 4]]
+    else:
+        df = df.iloc[:, [0, 1, 2, 3, 4, 8, 7, 6, 5]]
 
-    # Поменять столбцы местами
-    # Убрать 2 пустую строку
+    return df.drop(df.index[0])
+
+
+def main():
+    yahoo_tickers = ['DE', 'T', 'KS', 'SS', 'ME', 'HK', 'TW', 'JK', 'TO', 'IS', 'CO', 'KL', 'PA', 'SA', 'BK', 'SW',
+                     'MX', 'L', 'SN', 'OL', 'ST', 'SW', 'VI', 'MI', 'IL', 'AT', 'MC', 'LS', 'AX', 'ME', 'AS', 'DE',
+                     'BR', 'TO']
+    reports = ['Balance Sheet', 'Income Statement']
+
+    writer = pd.ExcelWriter('my_excel_file.xlsx', engine='xlsxwriter')
+
+    for ticker in yahoo_tickers:
+
+        balance_income_list = []
+        for report in reports:
+            balance_income_list.append(get_data_yahoo(report, ticker))
+
+
+
+    # df.to_excel(r'C:\Users\seriy_pv\PycharmProjects\pythonProject\Cpmpany_pars\Выгрузка тест.xlsx',
+    #             index=False)
 
 
 if __name__ == '__main__':
